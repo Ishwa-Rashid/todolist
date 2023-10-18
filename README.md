4dc7d768dcddb06959ba680f1187d4cb589c8060
Ishwa-Rashid <ishwarashid901@gmail.com>
2023-10-18 10:18:15 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# To-Do List Web Application
+
+This is a simple web application for managing your to-do lists. It's built using Bootstrap for the front-end, Node.js and Express for the back-end, and EJS as the templating engine. Additionally, it includes a "/work" route specifically for managing work-related tasks.
+
+## Features
+
+- Add tasks.
+- Mark tasks as complete.
+- Separate work-related tasks using the "/work" route.
+- Responsive design for a great user experience on all devices.
+
+## Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+- Node.js installed on your local machine.
+
+## Getting Started
+
+To get this project up and running on your local machine, follow these steps:
+
+1. Clone this repository:
+
+ ```bash
+   git clone https://github.com/Ishwa-Rashid/todolist-v1.git
+
+2. Change to the project directory:
+
+ ```bash
+   cd todolist-v1
+
+3. Install project dependencies:
+
+ ```bash
+   npm install
+
+4. Start the server:
+
+ ```bash
+   node app.js
+
+5. Open your web browser and visit http://localhost:3000 to access the to-do list application.
---
+++ app.js
@@ -0,0 +1,71 @@
+// requiring packages
+const express = require('express')
+const bodyParser = require('body-parser')
+const date = require(__dirname +'/date.js')
+// app  (handles requests) object
+const app = express()
+
+// to use body-parser
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+// will set the app's view engine to ejs (configuration settings)
+
+app.set('view engine', 'ejs')
+
+// same as 
+// app.use('view engine', 'ejs')
+
+const items = ['Buy Food', 'Cook Food', 'Eat Food']
+const workList = []
+app.get('/', function (req, res) {
+
+
+    const day = date.getDay()
+    
+    res.render('list', {listTitle:day,items:items})
+})
+
+app.post('/', function (req, res){
+    
+    const item = req.body.newItem
+    if(req.body.listName = 'Work'){
+        workList.push(item)
+        res.redirect('/work')
+    }else{
+
+        items.push(item)
+        res.redirect('/')
+    }
+
+})
+
+app.get('/work', function(req, res){
+
+    res.render('list',{listTitle:'Work List', items:workList})
+
+
+})
+
+
+app.listen(3000, function () {
+    console.log('Server is listening on port 3000')
+})
+
+
+
+
+
+
+
+
+
+
+
+// app.set(name, data) stores a named property on the app object that can be retrieved later with app.get(name).
+// Some property names for app.set() have predetermined effects that are described in the Express doc
+// and work like configuration options.
+
+// app.use() registers a middleware callback that will be part of the request handler chain for incoming http requests.
+// Depending upon the arguments, the middleware will either be called for
+// all incoming requests or only for certain requests.
